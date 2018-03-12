ad4e22a998dd39a522905a8110437cf370b25e7e
Joseph Shaw <j.g.shaw95@gmail.com>
2018-03-12 21:34:50 +0000

    Remove commented code

--- huffman-encoding/tests.hs
+++ huffman-encoding/tests.hs
@@ -8,9 +8,3 @@ main = hspec $ do
   describe "Huffman" $ do
     it "encode / decode test" $
       property $ \x -> decode (fst $ encode x) (snd $ encode x) == (x :: String)
-    {- it "can encode a string" $ do -}
-      {- snd (encode "aaaabcc") `shouldBe` "0000101111" -}
-    
-    {- it "can decode a string" $ do -}
-      {- let tree = Node (Leaf 'a' 4) (Node (Leaf 'b' 1) (Leaf 'c' 2)) -}
-      {- decode tree "0000101111" `shouldBe` "aaaabcc" -}
