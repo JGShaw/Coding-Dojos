d812546c9deea180134e3eae45280f115d47f704
Joseph Shaw <j.g.shaw95@gmail.com>
2018-07-20 15:30:46 +0100

    Remove brackets

--- highest-scoring-word/lib/highest_scoring_word.ex
+++ highest-scoring-word/lib/highest_scoring_word.ex
@@ -2,13 +2,13 @@ defmodule HighestScoringWord do
   def high(str) do
     words = String.split(str, " ")
     pairs = Enum.zip(words, Enum.map(words, &score_for_word/1))
-    Enum.max_by(pairs, &(elem(&1, 1)))
+    Enum.max_by(pairs, & elem(&1, 1))
     |> elem(0)
   end
 
   def score_for_word(word) do
     String.to_charlist(word)
-    |> Enum.map(&(&1 - (?a - 1)))
+    |> Enum.map(& &1 - (?a - 1))
     |> Enum.sum
   end
 end
