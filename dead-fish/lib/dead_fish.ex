97ba9fe17541f6aae912ddd8db153b94f37cd6ad
Joseph Shaw <j.g.shaw95@gmail.com>
2018-08-03 14:20:30 +0100

    Convert to case

--- dead-fish/lib/dead_fish.ex
+++ dead-fish/lib/dead_fish.ex
@@ -2,10 +2,13 @@ defmodule DeadFish do
   def parse(input), do: parse_list(String.codepoints(input), 0)
 
   def parse_list([], _), do: []
-  def parse_list(["i" | tail], num), do: parse_list(tail, num + 1) 
-  def parse_list(["d" | tail], num), do: parse_list(tail, num - 1)
-  def parse_list(["s" | tail], num), do: parse_list(tail, num * num)
-  def parse_list(["o" | tail], num), do: [num | parse_list(tail, num)]
-  def parse_list([_ | tail], num), do: parse_list(tail, num)
-
+  def parse_list([head | tail], num) do
+    case head do
+      "i" -> parse_list(tail, num + 1) 
+      "d" -> parse_list(tail, num - 1)
+      "s" -> parse_list(tail, num * num)
+      "o" -> [num | parse_list(tail, num)]
+      _ -> parse_list(tail, num)
+    end
+  end
 end
