d8edd992fc8cbcf4e4a54c4c5371e71385355f90
Joseph Shaw <j.g.shaw95@gmail.com>
2018-08-03 15:08:55 +0100

    inline tests

--- dead-fish/lib/dead_fish.ex
+++ dead-fish/lib/dead_fish.ex
@@ -1,10 +1,10 @@
 defmodule DeadFish do
-  def parse(input), do: parse_list(String.codepoints(input), 0)
+  def parse(input), do: String.codepoints(input) |> parse_list(0)
 
   def parse_list([], _), do: []
   def parse_list([head | tail], num) do
     case head do
-      "i" -> parse_list(tail, num + 1) 
+      "i" -> parse_list(tail, num + 1)
       "d" -> parse_list(tail, num - 1)
       "s" -> parse_list(tail, num * num)
       "o" -> [num | parse_list(tail, num)]
--- dead-fish/test/dead_fish_test.exs
+++ dead-fish/test/dead_fish_test.exs
@@ -2,21 +2,10 @@ defmodule DeadFishTest do
   use ExUnit.Case
   doctest DeadFish
 
-  test "ex1" do
-    assert DeadFish.parse("ooo") == [0,0,0]
-  end
-
-  test "ex2" do
-    assert DeadFish.parse("ioioio") == [1,2,3]
-  end
-
-  test "ex3" do
-    assert DeadFish.parse("idoiido") == [0,1]
-  end
-  test "ex4" do
-    assert DeadFish.parse("isoisoiso") == [1,4,25]
-  end
-  test "ex5" do
-    assert DeadFish.parse("codewars") == [0]
-  end
+  test "ex1", do: assert DeadFish.parse("ooo") == [0,0,0]
+  test "ex2", do: assert DeadFish.parse("ioioio") == [1,2,3]
+  test "ex3", do: assert DeadFish.parse("idoiido") == [0,1]
+  test "ex4", do: assert DeadFish.parse("isoisoiso") == [1,4,25]
+  test "ex5", do: assert DeadFish.parse("codewars") == [0]
+  test "no o reuturn empty", do: assert DeadFish.parse("iisii") == []
 end
